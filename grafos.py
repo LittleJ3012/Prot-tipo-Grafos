c1345eb0bbc8600f89557a8dc6c17651130a566a
Julia Barcellos Paiva <105679238+LittleJ3012@users.noreply.github.com>
2025-03-25 18:55:37 -0300

    Update grafos.py

--- grafos.py
+++ grafos.py
@@ -6,50 +6,54 @@ from google.colab import drive #Para fazer a conexão com os arquivos (ponte.txt
 # Montando o Google Drive
 drive.mount('/content/drive')
 
-#Esta função lê o arquivo.txt (passado como parâmetro pelo usuário) e armazena o conteúdo do arquivo em uma matriz do tipo numpy.
 
-def MatrizDeGrafos(inst): #O parâmetro é o nome do dataset(inst) 
+#A primeira função lê o arquivo.txt (passado como parâmetro pelo usuário) e armazena o conteúdo do arquivo em uma matriz do tipo numpy. Ela também
+#retorna a matriz e seu tipo.
+def LendoOsDados(inst): #O parâmetro é o nome do dataset(inst) 
     try:
 
-        # Uso do condicional if para que o programa saiba o caminho de cada um dos três arquivos no Google Drive:
-        if inst == "ponte":
-           caminho_arquivo = "/content/drive/My Drive/ponte.txt"
-        elif inst == "zachary":
-           caminho_arquivo = "/content/drive/My Drive/zachary.txt"
-        elif inst == "exemplo":
-           caminho_arquivo = "/content/drive/My Drive/exemplo.txt"
-        else:
-           print("\nArquivo inválido.") #Imprime uma mensagem de validação caso o usuário digite um nome inválido.
-           return None
+        # Definindo o caminho para o arquivo:
+        caminho_arquivo = "/content/drive/My Drive/" + inst + '.txt'
 
         # Carregar a matriz diretamente do arquivo no caminho completo
         matriz = np.loadtxt(caminho_arquivo)
 
         if isinstance(matriz, np.ndarray):  # Verifica se é uma matriz numpy
-            print("\nTipo da matriz:", type(matriz)) #Imprime o tipo da matriz
-            print("\nMatriz:")
-            print(matriz)
-
-            qtd_linhas, qtd_colunas = matriz.shape #Uso da função shape do Python para obter as dimensões da matriz, que é um objeto Numpy Array.
-
-            #Retornando as quantidades de linhas e colunas:
-            print(f"\nQuantidade de linhas: {qtd_linhas}")
-            print(f"\nQuantidade de colunas: {qtd_colunas}")
-            
-            #Salvando os resultados em um arquivo no Google Drive:
-            with open(f"/content/drive/My Drive/resultado_{inst}.txt", "w") as saida_arquivo:
-                saida_arquivo.write(f"{inst} {qtd_linhas} {qtd_colunas}\n") #Salvando o nome da instância e a dimensão da matriz
-            print(f"\nResultado salvo em: /content/drive/My Drive/resultado_{inst}.txt")
+           print("\nTipo da matriz:", type(matriz)) #Imprime o tipo da matriz
+           print(f"\nMatriz:\n\n{matriz}\n")
+           return matriz
 
         else:
-            print("O arquivo não gerou uma matriz numpy.")
+            print("O arquivo não gerou uma matriz numpy.") #Mensagem de validação.
 
     except Exception as e:
         print(f"Erro ao carregar o arquivo: {e}") #Mensagem de erro caso o programa não encontre o arquivo
         return None
 
 
-#Código do input para o usuário e a chamada das funções:
+#A segunda função calcula a dimensão da matriz, retornando a quantidade de linhas e colunas.
+def calculaDimensaoMatriz(matriz):
+    qtd_linhas, qtd_colunas = matriz.shape #Uso da função shape do Python para obter as dimensões da matriz, que é um objeto Numpy Array.
 
-nomeArquivo = input("\nDigite o nome do arquivo:")
-MatrizDeGrafos(nomeArquivo) # Chamada da função com o arquivo de parâmetro
+    #Retornando as quantidades de linhas e colunas:
+    return qtd_linhas, qtd_colunas
+
+            
+  #A terceira função salva os dados sobre as linhas e as colunas em um arquivo no Google Drive:
+def salvaResultado(inst, qtd_linhas, qtd_colunas):
+            with open(f"/content/drive/My Drive/resultado_{inst}.txt", "w") as saida_arquivo:
+                saida_arquivo.write(f"{inst} {qtd_linhas} {qtd_colunas}\n") #Salvando o nome da instância e a dimensão da matriz
+            print(f"\nResultado salvo em: resultado_{inst}.txt")
+
+
+
+#Definição da main:
+if __name__ == '__main__':
+
+#Código do input para o usuário e a chamada das funções:
+   inst = input("\nDigite o nome do arquivo:")
+   matriz = LendoOsDados(inst) # Chamada da função que retorna a matriz e seu tipo
+   if matriz is not None:
+        qtd_linhas, qtd_colunas = calculaDimensaoMatriz(matriz)  # Obtém as dimensões
+        print(f"\nDimensões da matriz: {qtd_linhas} linhas x {qtd_colunas} colunas")  # Imprime as dimensões na tela
+        salvaResultado(inst, qtd_linhas, qtd_colunas)  # Chamada da função que salva as dimensões
